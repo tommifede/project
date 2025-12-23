be13631d7e6e3f0d3c780831b7773aab58eb19d6
tommifede <tommaso.federici3@studio.unibo.it>
2025-12-23 09:57:50 +0100

    Modified files: .clang-format

--- src/simulation.cpp
+++ src/simulation.cpp
@@ -1,3 +1,16 @@
 #include "simulation.hpp"
+#include <iostream>
 
-namespace lotka_volterra {} // namespace lotka_volterra
+namespace lotka_volterra {
+Simulation::Simulation(double x0 = 0., double y0 = 0., double dt)
+    : x_rel_{x0}
+    , y_rel_{y0}
+    , dt_{dt}
+{
+  if (x0 < 0. || y0 < 0. || dt < 0.) {
+    std::cerr << "Error: x0, y0 and dt must be >= 0!\n";
+    std::exit(EXIT_FAILURE);
+  }
+  states_.push_back({x_rel_, y_rel_, compute_H(x_rel_, y_rel_)});
+}
+} // namespace lotka_volterra
--- src/simulation.hpp
+++ src/simulation.hpp
@@ -6,7 +6,36 @@
 
 namespace lotka_volterra {
 class Simulation
-{};
+{
+public:
+  struct State
+  {
+    double x;
+    double y;
+    double H;
+  };
+
+  Simulation(double x0, double y0, double dt);
+  Simulation(double A, double B, double C, double D, double x0, double y0,
+             double dt);
+
+
+  void evolve();
+  std::size_t steps() const;
+  State const& state(std::size_t i) const;
+
+private:
+  double A_{0.};
+  double B_{0.};
+  double C_{0.};
+  double D_{0.};
+  double dt_;
+  double x_rel_;
+  double y_rel_;
+  std::vector<State> states_;
+  double compute_H(double x, double y) const;
+  void integrate();
+};
 } // namespace lotka_volterra
 
 #endif
