24efa79b894361f6c74cabf02bd82b34af6c80e1
tommifede <tommaso.federici3@studio.unibo.it>
2025-12-24 14:49:22 +0100

    Modified main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
+#include "../include/output.hpp"
 #include "../include/renderer.hpp"
 #include "../include/simulation.hpp"
-#include "../include/output.hpp"
 #include <SFML/Graphics.hpp>
 #include <string>
 
@@ -9,10 +9,10 @@ int main()
   int const width  = 800;
   int const height = 600;
 
-  sf::RenderWindow window1(sf::VideoMode(width, height), "Lotka-Volterra 1");
+  /* sf::RenderWindow window1(sf::VideoMode(width, height), "Lotka-Volterra 1");
 
-  lotka_volterra::Simulation simulation1(0.001, 1., 1., 1., 1., 10., 5.);
-  simulation1.evolve_time(5.);
+  lotka_volterra::Simulation simulation1(0.0001, 2., 0.1, 0.1, 1., 5.5, 10.);
+  simulation1.evolve_time(100.);
 
   io::output_csv(simulation1, "trajectory1.csv");
 
@@ -25,13 +25,15 @@ int main()
       }
     }
     window1.clear(sf::Color::White);
+    renderer1.draw_axes(window1, width, height);
+    renderer1.draw_ticks(window1, width, height);
     renderer1.draw(window1, simulation1);
     window1.display();
-  }
+  } */
 
   sf::RenderWindow window2(sf::VideoMode(width, height), "Lotka-Volterra 2");
 
-  lotka_volterra::Simulation simulation2(0.001, 1., 1., 1., 1., 10., 5.);
+  lotka_volterra::Simulation simulation2(0.01, 2., 0.1, 0.1, 1., 8, 10.);
 
   int step = 0;
   lotka_volterra::Renderer renderer2(width, height);
@@ -47,7 +49,7 @@ int main()
     else
       simulation2.evolve();
 
-    window2.clear();
+    window2.clear(sf::Color::White);
     renderer2.draw_axes(window2, width, height);
     renderer2.draw_ticks(window2, width, height);
     renderer2.draw(window2, simulation2, step);
