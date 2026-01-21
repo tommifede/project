ddde4c5f26ce9b5156c1481a7e17ea44b0faba81
tommifede <tommaso.federici3@studio.unibo.it>
2026-01-21 19:43:32 +0100

    Updated output

--- io/output.cpp
+++ io/output.cpp
@@ -11,10 +11,10 @@ void output_csv(lotka_volterra::Simulation const& simulation,
     throw std::runtime_error("cannot open file");
   }
 
-  file << "step,x,y,H\n";
+  file << "t,x,y,H\n";
   for (std::size_t i = 0; i < simulation.steps(); ++i) {
     lotka_volterra::State const& state = simulation.stateAt(i);
-    file << i << "," << state.x << "," << state.y << "," << state.H << "\n";
+    file << i*simulation.dt() << "," << state.x << "," << state.y << "," << state.H << "\n";
   }
 }
 } // namespace io
